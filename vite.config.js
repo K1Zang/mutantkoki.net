a0912a7316283cd1ac32dcfd426f883f9f855f5f
K1Zang <keivanpourzang@gmail.com>
2023-11-15 23:48:55 +0300

    d

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/mutantkoki.net/',
+  base: '/mutantkoki.net/',
 
   plugins: [vue()],
   resolve: {
